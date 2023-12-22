6c06f34ad2dd8328ca0a1e1fbf6f6df174969bb7
steif <you@example.com>
2023-12-22 22:21:42 +0800

    fix

--- src/random.rs
+++ src/random.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::{convert::TryInto, ops::Add};
 
 use crate::{generator, seeds::generate_seed};
 
@@ -11,8 +11,22 @@ pub(crate) fn random(seed: u256, min: u128, max: u128) -> u128 {
 
     let mut input: Vec<u64> = vec![];
 
-    // let high: u128 = seed.0[3].into() << 64 + seed.0[2];
-    // let low: u128 = seed.0[1].into() << 64 + seed.0[0];
+    let high: u128 = ((seed.0[3] as u128) << 64 + seed.0[2]).reverse_bits();
+    let low: u128 = ((seed.0[1] as u128) << 64 + seed.0[0]).reverse_bits();
+
+    let be_bytes = high.to_be_bytes();
+    let (high_in_high, low_in_high) = be_bytes.split_at(64);
+
+    input.push(u64::from_be_bytes(low_in_high.try_into().unwrap()));
+    input.push(u64::from_be_bytes(high_in_high.try_into().unwrap()));
+
+    let be_bytes = low.to_be_bytes();
+    let (high_in_low, low_in_low) = be_bytes.split_at(64);
+
+    input.push(u64::from_be_bytes(low_in_low.try_into().unwrap()));
+    input.push(u64::from_be_bytes(high_in_low.try_into().unwrap()));
+
+    println!("input: {:?}", input);
 
     0
 }
