04ba9037c0d1f6f0eb76cb11ac3d90a4c39e0622
Steif <you@example.com>
2024-01-14 20:08:59 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,27 @@ construct_uint! {
 
 fn main() {
     println!("Hold your torch here!");
-    let id: u32 = {
-        let mut input = String::new();
-        std::io::stdin()
-            .read_line(&mut input)
-            .expect("Strange input!");
-        input.trim().parse().expect("Give me a number!")
-    };
-    let cc: CryptsAndCaverns = (if id < 1 || id > 9000 {
-        println!("Oh, let's generate a random one...");
-        seeds::generate_seed()
-    } else {
-        println!("Your input ID was: {}", id);
-        seeds::get_seed(id)
-    })
-    .generate_map();
-    println!("{:?}", &cc);
+    loop {
+        println!("- type a number to generate a Crypts and Caverns");
+        println!("- type any characters to quit");
+        let id: u32 = {
+            let mut input = String::new();
+            std::io::stdin()
+                .read_line(&mut input)
+                .expect("Strange input!");
+            match input.trim().parse() {
+                Ok(num) => num,
+                Err(_) => break,
+            }
+        };
+        let cc: CryptsAndCaverns = (if id < 1 || id > 9000 {
+            println!("Oh, let's generate a random one...");
+            seeds::generate_seed()
+        } else {
+            println!("Your input is: {}", id);
+            seeds::get_seed(id)
+        })
+        .generate_map();
+        println!("{:?}", &cc);
+    }
 }
